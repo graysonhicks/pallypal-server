15aade5703ab0df4119d8881a95f58709540f7ff
Grayson Hicks <graysonhicks@gmail.com>
2017-09-07 14:15:23 -0400

    basic filesystem set up with stream

--- index.js
+++ index.js
@@ -21,7 +21,53 @@ app.use(cors()); //allows overriding cross origin policy (use npm install if nee
 // }
 app.get("/build", function(req, res) {
 	// listens for request on /build route
+	var css;
+    var fileName;
+	switch (req.type) {
+		case "css":
+        function() {
+		    css = buildCSS(req.colors);
+            fileName = "/colors.css";
+		}
+			break;
+		case "scss":
+        function() {
+		    css = buildSCSS(req.colors);
+            fileName = "/colors.scss";
+		}
+			break;
+		case "sass":
+        function() {
+		    css = buildSASS(req.colors);
+            fileName = "/colors.sass";
+		}
+			break;
+		default:
+        function(){
+            css = buildCSS(req.colors);
+            fileName = "/colors.css";
+        }
+	}
+
+
+    var stream = fs.createWriteStream(fileName);
+    stream.once('open', function(fd) {
+      stream.end(css);
+    });
+
 });
+
+function buildCSS(req) {
+	return "I am a CSS file!";
+}
+
+function buildSCSS(req) {
+	return "I am an SCSS file!";
+}
+
+function buildSASS(req) {
+	return "I am a SASS file!";
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port);

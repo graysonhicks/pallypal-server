db678f8eeecb79aa8f92e404f0fafe150ffd14ec
Grayson Hicks <graysonhicks@gmail.com>
2017-09-07 23:26:07 -0400

    cors header

--- index.js
+++ index.js
@@ -31,11 +31,10 @@ app.get("/", function(req, res) {
 	res.send("Go to <a href='https://graysonhicks.github.io/pallypal'>Pallypal</a> to build your palette.");
 });
 
-app.post("/build", function(req, res) {
+app.get("/build", function(req, res) {
 	// listens for request on /build route
-	var body = req.body;
-	var colors = body.colors;
-	var type = body.type;
+	var colors = res.query.colors;
+	var type = res.query.type;
 	var css;
 	switch (type) {
 		case "css":
@@ -51,27 +50,15 @@ app.post("/build", function(req, res) {
 			css = buildCSS(colors);
 	}
 
-fs.writeFile('colors' + .type, css, 'utf8', function (err) {
-  if (err) {
-    console.log('Some error occured - file either not saved or corrupted file saved.');
-  } else{
-    console.log('It\'s saved!');
-  }
+res.setHeader("Content-disposition", "attachment; filename=colors." + type);
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader("Content-type", "text/css");
+res.charset = "UTF-8";
+res.write(css);
+res.end();
 });
 
-});
 
-app.get("/build", function(req, res) {
-	// listens for request on /build route
-
-	var type = req.query.type;
-
-	res.setHeader("Content-disposition", "attachment; filename=colors." + type);
-	res.setHeader("Content-type", "text/css");
-	res.charset = "UTF-8";
-	res.write(css);
-	res.end();
-});
 
 function buildCSS(colors) {
 	var text = "";
